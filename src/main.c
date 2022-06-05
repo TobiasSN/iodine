b3264484ec6d2bc15d07d316e069a724ca1f021f
Tobias SN <15670490+TobiasSN@users.noreply.github.com>
2022-06-05 23:11:07 +0200

    chore: remove stivale2

--- src/main.c
+++ src/main.c
@@ -1,12 +1,8 @@
 // main.c - The kernel entry point.
 
-#include <stddef.h>
-#include "../third-party/stivale/stivale2.h"
 #include "idt.h"
 #include "interrupts.h"
 #include "log.h"
-
-void (*term_write)(const char* string, size_t length);
 
 void _start() {
 	log_string("Welcome to Iodine!\n");
